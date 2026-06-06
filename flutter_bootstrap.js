6ab257b6c8d2986497a9e96986d2b4cd4e77bf56
A K <ein@biglilly.local>
2026-06-06 14:09:19 +0700

    Deploy fosy0 web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3025502266" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3678528780" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
